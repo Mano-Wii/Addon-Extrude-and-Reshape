09c4317bd2966cfac9cdc25f4a9351b9c866a6e9
Germano Cavalcante de Sousa <germano.costa@ig.com.br>
2015-08-03 21:37:01 -0300

    Tests intersection between new edges and all edges
    Now also creates a vertex on the edges intersecting other edges in general.

--- mesh_push_pull_face.py
+++ mesh_push_pull_face.py
@@ -22,11 +22,11 @@
 bl_info = {
     "name": "Push Pull Face",
     "author": "Germano Cavalcante",
-    "version": (0, 4),
+    "version": (0, 5),
     "blender": (2, 75, 0),
     "location": "View3D > TOOLS > Tools > Mesh Tools > Add: > Extrude Menu (Alt + E)",
     "description": "Push and pull face entities to sculpt 3d models",
-    #"wiki_url" : "http://blenderartists.org/forum/",
+    "wiki_url" : "http://blenderartists.org/forum/showthread.php?376618-Addon-Push-Pull-Face",
     "category": "Mesh"}
 
 import bpy
@@ -34,206 +34,103 @@ import bmesh
 from mathutils import Vector
 from bpy.props import FloatProperty
 
-def get_coplanar_faces(bm, face, precision = 4):
-    faces = []
-    nn = -face.normal
-    normals = face.normal.to_tuple(precision), nn.to_tuple(precision)
-    m_face = face.calc_center_median()
-    for f in bm.faces:
-        #if f != face:
-        n = f.normal.to_tuple(precision)
-        if n in normals:
-            vec = f.calc_center_median() - m_face
-            if abs(vec.dot(n)) < 0.001 and f!= face:
-                faces.append(f)
-    return faces
-
-def intersect_bound_box_face_edge(face, edge, precision = 4):
-    l_f = 2*len(face.verts)
-    x1 = 0
-    x0 = 0
-    y1 = 0
-    y0 = 0
-    z1 = 0
-    z0 = 0
-    for point in edge.verts:
-        for v in face.verts:
-            v = v.co - point.co
-            dx, dy, dz = v.to_tuple(precision)
-            if dx < 0:
-                x0 += 1
-            elif dx > 0:
-                x1 += 1
+def intersect_bound_box_edge_edge(a1, a2, b1, b2, precision = 0.0001):
+    bbx1 = sorted([a1.x, a2.x])
+    bbx2 = sorted([b1.x, b2.x])
+    if ((bbx1[0] - precision) <= bbx2[1]) and (bbx1[1] >= (bbx2[0] - precision)):
+        bby1 = sorted([a1.y, a2.y])
+        bby2 = sorted([b1.y, b2.y])
+        if ((bby1[0] - precision) <= bby2[1]) and (bby1[1] >= (bby2[0] - precision)):
+            bbz1 = sorted([a1.z, a2.z])
+            bbz2 = sorted([b1.z, b2.z])
+            if ((bbz1[0] - precision) <= bbz2[1]) and (bbz1[1] >= (bbz2[0] - precision)):
+                return True
             else:
-                x0 -= 1
-                x1 -= 1
+                return False
+        else:
+            return False
+    else:
+        return False
 
-            if dy < 0:
-                y0 += 1
-            elif dy > 0:
-                y1 += 1
-            else:
-                y0 -= 1
-                y1 -= 1
-
-            if dz < 0:
-                z0 += 1
-            elif dz > 0:
-                z1 += 1
-            else:
-                z0 -= 1
-                z1 -= 1
-    inside = True
-    if x0 >= l_f or x1 >= l_f:
-        inside = False
-    if y0 >= l_f or y1 >= l_f:
-        inside = False
-    if z0 >= l_f or z1 >= l_f:
-        inside = False
-    return inside
-
-def intersect_bound_box_edge_edge(edge1, edge2, precision = 4):
-    x1 = 0
-    x0 = 0
-    y1 = 0
-    y0 = 0
-    z1 = 0
-    z0 = 0    
-    for v1 in edge1.verts:
-        for v2 in edge2.verts:
-            v = (v1.co - v2.co)
-            dx, dy, dz = v.to_tuple(precision)
-            if dx < 0:
-                x0 += 1
-            elif dx > 0:
-                x1 += 1
-            else:
-                x0 -= 1
-                x1 -= 1
-
-            if dy < 0:
-                y0 += 1
-            elif dy > 0:
-                y1 += 1
-            else:
-                y0 -= 1
-                y1 -= 1
-
-            if dz < 0:
-                z0 += 1
-            elif dz > 0:
-                z1 += 1
-            else:
-                z0 -= 1
-                z1 -= 1
-    inside = True
-    if x0 >= 4 or x1 >= 4:
-        inside = False
-    if y0 >= 4 or y1 >= 4:
-        inside = False
-    if z0 >= 4 or z1 >= 4:
-        inside = False
-    return inside
-
-def intersect_face_edges(face, edges, ignore = {}, precision = 4):
-    new_edges = []
-    new_face_edges = []
+def intersect_edges_edges(edges1, edges2, ignore = {}, precision = 4):
+    fprec = .1**precision
+    new_edges1 = set()
+    new_edges2 = set()
     targetmap = {}
     exclude = {}
-    for edge in edges:
-        if edge in face.edges:
-            intersect = True
-        else:
-            intersect = intersect_bound_box_face_edge(face, edge, precision = precision) #desnecessário para edge na face
-        if intersect:
-            for fedg in [e for e in face.edges if e not in edges]:
-                if edge not in ignore or fedg not in ignore[edge]:
-                    intersect2 = intersect_bound_box_edge_edge(edge, fedg, precision = precision)
-                    if intersect2:
-                        #print(edge.index, fedg.index)
-                        ve0 = edge.verts[0]
-                        vf0 = fedg.verts[0]
+    for ed1 in edges1:
+        for ed2 in edges2:
+            if ed1 != ed2 and (ed1 not in ignore or ed2 not in ignore[ed1]):
+                a1 = ed1.verts[0]
+                a2 = ed1.verts[1]
+                b1 = ed2.verts[0]
+                b2 = ed2.verts[1]
+                intersect = intersect_bound_box_edge_edge(a1.co, a2.co, b1.co, b2.co, precision = fprec)
+                if intersect:
+                    v1 = a2.co-a1.co
+                    v2 = b2.co-b1.co
+                    v3 = a1.co-b1.co
+                    
+                    cross1 = v3.cross(v1)
+                    cross2 = v3.cross(v2)
+                    
+                    crosscross = cross1.cross(cross2)
+                    if crosscross.to_tuple(2) == (0,0,0): #cross cross is very inaccurate
+                        cross3 = v2.cross(v1)
+                        lc3 = cross3.x+cross3.y+cross3.z
 
-                        v1 = ve0.co
-                        v2 = edge.verts[1].co
-                        v3 = vf0.co
-                        v4 = fedg.verts[1].co
-
-                        c1 = (v3-v1).cross(v4-v1)
-                        c2 = (v3-v2).cross(v4-v2)
-                        c3 = (v1-v3).cross(v2-v3)
-                        c4 = (v1-v4).cross(v2-v4)
-
-                        c1l = (c1.x + c1.y + c1.z)
-                        c2l = -(c2.x + c2.y + c2.z)
-                        c3l = (c3.x + c3.y + c3.z)
-                        c4l = -(c4.x + c4.y + c4.z)
-                        
-                        l = [c1l,c2l,c3l,c4l]
-                        zeros = l.count(0)
-
-                        if zeros == 0 and\
-                           (c1l > 0) == (c2l > 0) and\
-                           (c3l > 0) == (c4l > 0):
-
-                            f1 = c1l/(c1l+c2l)
-                            f2 = c3l/(c3l+c4l)
-
-                            ef, vf = bmesh.utils.edge_split(fedg, vf0, f2)
-                            new_face_edges += [fedg, ef]
-
-                            e, v = bmesh.utils.edge_split(edge, ve0, f1)
-                            new_edges += [edge, e]
-
-                            exclude[edge] = exclude[e] = {fedg, ef}
-
-                            targetmap[v] = vf
-
-                        elif zeros == 1: #Será que não precisa testar cross por causa do intersect_bound_box_edge_edge???
-                            if 0 not in {c1l,c2l} and (c1l > 0) != (c2l > 0):
-                                continue
-                            if 0 not in (c3l,c4l) and (c3l > 0) != (c4l > 0):
-                                continue
-                                
-                            set_ign = {fedg}
-
-                            f1 = c1l/(c1l+c2l)
-                            f2 = c3l/(c3l+c4l)
+                        if abs(lc3) > fprec: 
+                            lc1 = cross1.x+cross1.y+cross1.z
+                            lc2 = cross2.x+cross2.y+cross2.z
                             
-                            if f2 not in {0, 1}:
-                                ef, vf = bmesh.utils.edge_split(fedg, vf0, f2)
-                                new_face_edges += [fedg, ef]
-                                set_ign.add(ef)
-                            elif f2 == 0:
-                                vf = vf0
-                            else:
-                                vf = fedg.verts[1]
+                            fac1 = lc2/lc3
+                            fac2 = lc1/lc3
+                            if 0 <= fac1 <= 1 and 0 <= fac2 <= 1:
+                                rfac1 = round(fac1, precision)
+                                rfac2 = round(fac2, precision)
+                                set_ign = {ed2}
 
-                            if f1 not in {0, 1}:
-                                e, v = bmesh.utils.edge_split(edge, ve0, f1)
-                                new_edges += [edge, e]
-                                exclude[edge] = exclude[e] = set_ign
-                            elif f1 == 0:
-                                v = ve0
-                            else:
-                                v = edge.verts[1]
+                                if 0 < rfac2 < 1:
+                                    ne2, nv2 = bmesh.utils.edge_split(ed2, b1, fac2)
+                                    new_edges2.update({ed2, ne2})
+                                    set_ign.add(ne2)
+                                elif rfac2 == 0:
+                                    nv2 = b1
+                                else:
+                                    nv2 = b2
 
-                            targetmap[v] = vf
-                            
-                        else:
-                            pass
-                            #print('colienares')
-                            
-    if new_edges or new_face_edges:
-        ned, tar = intersect_face_edges(face, new_edges, ignore = exclude, precision = precision)
+                                if 0 < rfac1 < 1:
+                                    ne1, nv1 = bmesh.utils.edge_split(ed1, a1, fac1)
+                                    new_edges1.update({ed1, ne1})
+                                    exclude[ed1] = exclude[ne1] = set_ign
+                                elif rfac1 == 0:
+                                    nv1 = a1
+                                    exclude[ed1] = set_ign
+                                else:
+                                    nv1 = a2
+                                    exclude[ed1] = set_ign
+
+                                if nv1 != nv2:
+                                    targetmap[nv1] = nv2
+                            #else:                            
+                                #print('not intersect')
+                        #else:
+                            #print('colinear')
+                    #else:
+                        #print('not coplanar')
+    if new_edges1 or new_edges2:
+        edges1.update(new_edges1)
+        ned, tar = intersect_edges_edges(edges1, new_edges2, ignore = exclude, precision = precision)
         if tar != targetmap:
+            new_edges1.update(ned["new_edges1"])
+            new_edges2.update(ned["new_edges2"])
             targetmap.update(tar)
-        return {"new_face_edges": new_face_edges + ned["new_face_edges"],
-                "new_edges": new_edges + ned["new_face_edges"]
+        return {"new_edges1": new_edges1,
+                "new_edges2": new_edges2
                 }, targetmap
     else:
-        return {"new_face_edges": new_face_edges,
-                "new_edges": new_edges
+        return {"new_edges1": new_edges1,
+                "new_edges2": new_edges2
                 }, targetmap
 
 class Push_Pull_Face(bpy.types.Operator):
@@ -242,64 +139,26 @@ class Push_Pull_Face(bpy.types.Operator):
     bl_label = "Push/Pull Face"
     bl_options = {'REGISTER', 'GRAB_CURSOR', 'BLOCKING'}
 
-    angle_tolerance = FloatProperty(
-            name="precision orthogonality",
-            description="orthogonality tolerance of a face relative to another.",
-            min=0.000000, max=3.141592653589793,
-            default=0.01,
-            )
-
     @classmethod
     def poll(cls, context):
         return context.mode is not 'EDIT_MESH'
-    
-    def face_hierarchy(self):
-        self.bm.faces.ensure_lookup_table()
-        try:
-            sface = self.bm.faces[self.face_id]
-        except: #IndexError or ReferenceError
-            self.bm = bmesh.from_edit_mesh(self.mesh) # para ReferenceError
-            for face in self.bm.faces:
-                if face.select == True:
-                    sface = face
-                    break
-            else:
-                bmesh.update_edit_mesh(self.mesh, tessface=True, destructive=True)
-                return {'FINISHED'}
-        tface = {sface}
-        targetmap = {}
-        ignore = {}
-        for loop in sface.loops:
-            loop2 = loop.link_loops[0] #[0]!!!!!!!!!!!!!!!!!!!!!!!!
-            face = loop2.face
-            tface.add(face)            
-            if len(face.verts) != 4:
-                edges = [loop2.edge, loop2.link_loop_next.edge, loop2.link_loop_prev.edge]
-                new_edges, tmpmap = intersect_face_edges(face, edges, precision = 4)
-                targetmap.update(tmpmap)
-                for e in new_edges['new_face_edges']:
-                    ignore[e] = set()
-
-        for face in tface:
-            coplanar_faces = get_coplanar_faces(self.bm, face, precision = 4)
-            set_edges = set()
-            for f in coplanar_faces:
-                for edge in f.edges:
-                    set_edges.add(edge)
-            new_edges2, tmpmap2 = intersect_face_edges(face, set_edges, ignore = ignore, precision = 4)
-            targetmap.update(tmpmap2)
-
-        if targetmap:
-            bmesh.ops.weld_verts(self.bm, targetmap=targetmap)
-        bmesh.update_edit_mesh(self.mesh, tessface=True, destructive=True)
 
     def modal(self, context, event):
         op = context.window_manager.operators
         #print([o.name for o in op])
         if op and op[-1].name == 'Translate':
             #print('-------------------')
-            self.face_hierarchy()
-            #bpy.ops.mesh.intersect(mode='SELECT_UNSELECT', use_separate=True, threshold=0.01)
+            sface = self.bm.faces.active
+            set_edges = set()
+            for v in sface.verts:
+                for ed in v.link_edges:
+                    set_edges.add(ed)
+            bm_edges = set(self.bm.edges)#.difference_update(set_edges)
+            bm_edges.difference_update(set_edges)
+            new_edges, targetmap = intersect_edges_edges(set_edges, bm_edges)
+            if targetmap:
+                bmesh.ops.weld_verts(self.bm, targetmap=targetmap)
+            bmesh.update_edit_mesh(self.mesh, tessface=True, destructive=True)
             return {'FINISHED'}
         if self.cancel:
             return {'FINISHED'}
@@ -325,7 +184,6 @@ class Push_Pull_Face(bpy.types.Operator):
             face = selection
             geom = []
             for edge in face.edges:
-                #print(edge.calc_face_angle()- 1.5707963267948966)
                 if abs(edge.calc_face_angle(0) - 1.5707963267948966) < 0.01: #self.angle_tolerance:
                     geom.append(edge)
 
@@ -343,7 +201,7 @@ class Push_Pull_Face(bpy.types.Operator):
         rv3d = context.region_data
         rv3d.view_location = rv3d.view_location
         context.window_manager.modal_handler_add(self)
-        
+
         self.cancel = False
         return {'RUNNING_MODAL'}
 
