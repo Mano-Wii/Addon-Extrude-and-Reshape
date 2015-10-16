35d5650e0e2ccee8fe6e7dfb95d5cb2e6c598974
Germano Cavalcante <germano.costa@ig.com.br>
2015-10-16 11:49:56 -0300

    Cleanup

--- mesh_push_pull_face.py
+++ mesh_push_pull_face.py
@@ -68,28 +68,20 @@ def intersect_edges_edges(overlay, precision = 4):
     fpre_max = 1+epsilon
     splits = {}
     sp_get = splits.get
-    ignore = {}
-    ig_get = ignore.get
     new_edges1 = set()
     new_edges2 = set()
     targetmap = {}
-    for ed1 in overlay:
+    for edg1 in overlay:
         #print("***", ed1.index, "***")
-        edg1 = ed1
-        for ed2 in overlay[edg1]:
+        for edg2 in overlay[edg1]:
             #print('loop', ed2.index)
-            edg2 = ed2
-            ed1 = edg1
-
-            a1 = ed1.verts[0]
-            a2 = ed1.verts[1]
-
-            b1 = ed2.verts[0]
-            b2 = ed2.verts[1]
+            a1 = edg1.verts[0]
+            a2 = edg1.verts[1]
+            b1 = edg2.verts[0]
+            b2 = edg2.verts[1]
             
             # test if are linked
             if a1 in {b1, b2} or a2 in {b1, b2}:
-                ignore[ed1] = ig_get(ed1, set()).union({ed2})
                 #print('linked')
                 continue
 
@@ -98,102 +90,102 @@ def intersect_edges_edges(overlay, precision = 4):
             tp = intersect_line_line(aco1, aco2, bco1, bco2)
             if tp:
                 p1, p2 = tp
-            else:
-                print("parallel or collinear")
-                continue
+                if (p1 - p2).to_tuple(precision) == (0,0,0):
+                    v = aco2-aco1
+                    f = p1 - aco1
+                    x,y,z = abs(v.x), abs(v.y), abs(v.z)
+                    max1 = 0 if x >= y and x >= z else\
+                           1 if y >= x and y >= z else 2
+                    fac1 = f[max1]/v[max1]
 
-            if (p1 - p2).to_tuple(precision) == (0,0,0):
-                v = aco2-aco1
-                f = p1 - aco1
-                x,y,z = abs(v.x), abs(v.y), abs(v.z)
-                max1 = 0 if x >= y and x >= z else\
-                       1 if y >= x and y >= z else 2
-                fac1 = f[max1]/v[max1]
+                    v = bco2-bco1
+                    f = p2 - bco1
+                    x,y,z = abs(v.x), abs(v.y), abs(v.z)
+                    max2 = 0 if x >= y and x >= z else\
+                           1 if y >= x and y >= z else 2
+                    fac2 = f[max2]/v[max2]
 
-                v = bco2-bco1
-                f = p2 - bco1
-                x,y,z = abs(v.x), abs(v.y), abs(v.z)
-                max2 = 0 if x >= y and x >= z else\
-                       1 if y >= x and y >= z else 2
-                fac2 = f[max2]/v[max2]
+                    if fpre_min <= fac1 <= fpre_max:
+                        #print(edg1.index, 'can intersect', edg2.index)
+                        ed1 = edg1
+                        va1 = a1
+                        va2 = a2
 
-                if fpre_min <= fac1 <= fpre_max:
-                    #print(edg1.index, 'can intersect', edg2.index)
-                    pass
-                elif edg1 in splits:
-                    for e in splits[edg1]:
-                        a1 = e.verts[0]
-                        a2 = e.verts[1]
+                    elif edg1 in splits:
+                        for ed1 in splits[edg1]:
+                            va1 = ed1.verts[0]
+                            va2 = ed1.verts[1]
 
-                        aco1 = a1.co
-                        aco2 = a2.co
+                            vco1 = va1.co
+                            vco2 = va2.co
 
-                        v = aco2-aco1
-                        f = p1 - aco1
-                        fac1 = f[max1]/v[max1]
-                        if fpre_min <= fac1 <= fpre_max:
-                            #print(e.index, 'can intersect', edg2.index)
-                            ed1 = e
-                            break
+                            v = vco2-vco1
+                            f = p1 - vco1
+                            fac1 = f[max1]/v[max1]
+                            if fpre_min <= fac1 <= fpre_max:
+                                #print(e.index, 'can intersect', edg2.index)
+                                break
+                        else:
+                            #print(edg1.index, 'really does not intersect', edg2.index)
+                            continue
                     else:
-                        #print(edg1.index, 'really does not intersect', edg2.index)
+                        #print(edg1.index, 'not intersect', edg2.index)
                         continue
-                else:
-                    #print(edg1.index, 'not intersect', edg2.index)
-                    continue
 
-                if fpre_min <= fac2 <= fpre_max:
-                    #print(ed1.index, 'actually intersect', edg2.index)
-                    pass
-                elif edg2 in splits:
-                    for e in splits[edg2]:
-                        b1 = e.verts[0]
-                        b2 = e.verts[1]
+                    if fpre_min <= fac2 <= fpre_max:
+                        #print(ed1.index, 'actually intersect', edg2.index)
+                        ed2 = edg2
+                        vb1 = b1
+                        vb2 = b2
 
-                        bco1 = b1.co
-                        bco2 = b2.co
+                    elif edg2 in splits:
+                        for ed2 in splits[edg2]:
+                            vb1 = ed2.verts[0]
+                            vb2 = ed2.verts[1]
 
-                        v = bco2-bco1
-                        f = p2 - bco1
-                        fac2 = f[max2]/v[max2]
-                        if fpre_min <= fac2 <= fpre_max:
-                            #print(ed1.index, 'actually intersect', e.index)
-                            ed2 = e
-                            break
+                            vco1 = vb1.co
+                            vco2 = vb2.co
+
+                            v = vco2-vco1
+                            f = p2 - vco1
+                            fac2 = f[max2]/v[max2]
+                            if fpre_min <= fac2 <= fpre_max:
+                                #print(ed1.index, 'actually intersect', e.index)
+                                break
+                        else:
+                            #print(ed1.index, 'really does not intersect', ed2.index)
+                            continue
                     else:
-                        #print(ed1.index, 'really does not intersect', ed2.index)
+                        #print(ed1.index, 'not intersect', edg2.index)
                         continue
-                else:
-                    #print(ed1.index, 'not intersect', edg2.index)
-                    continue
 
-                ignore[ed1] = ig_get(ed1, set()).union({ed2})
-                new_edges1.add(ed1)
-                new_edges2.add(ed2)
+                    new_edges1.add(ed1)
+                    new_edges2.add(ed2)
 
-                if abs(fac1) <= epsilon:
-                    nv1 = a1
-                elif fac1 + epsilon >= 1:
-                    nv1 = a2
-                else:
-                    ne1, nv1 = bmesh.utils.edge_split(ed1, a1, fac1)
-                    new_edges1.add(ne1)
-                    splits[edg1] = sp_get(edg1, set()).union({ne1})
+                    if abs(fac1) <= epsilon:
+                        nv1 = va1
+                    elif fac1 + epsilon >= 1:
+                        nv1 = va2
+                    else:
+                        ne1, nv1 = bmesh.utils.edge_split(ed1, va1, fac1)
+                        new_edges1.add(ne1)
+                        splits[edg1] = sp_get(edg1, set()).union({ne1})
 
-                if abs(fac2) <= epsilon:
-                    nv2 = b1
-                elif fac2 + epsilon >= 1:
-                    nv2 = b2
-                else:
-                    ne2, nv2 = bmesh.utils.edge_split(ed2, b1, fac2)
-                    new_edges2.add(ne2)
-                    splits[edg2] = sp_get(edg2, set()).union({ne2})
+                    if abs(fac2) <= epsilon:
+                        nv2 = vb1
+                    elif fac2 + epsilon >= 1:
+                        nv2 = vb2
+                    else:
+                        ne2, nv2 = bmesh.utils.edge_split(ed2, vb1, fac2)
+                        new_edges2.add(ne2)
+                        splits[edg2] = sp_get(edg2, set()).union({ne2})
 
-                if nv1 != nv2:
-                    targetmap[nv1] = nv2
-
+                    if nv1 != nv2: #necessary?
+                        targetmap[nv1] = nv2
+                #else:
+                    #print('not coplanar')
             #else:
-                #print('not coplanar')
+                #print("parallel or collinear")
     return new_edges1, new_edges2, targetmap
 
 class Push_Pull_Face(bpy.types.Operator):
